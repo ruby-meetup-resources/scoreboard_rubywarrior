fcf25b6ac2044d33ce4fb55d0de05d6d5d789da6
Patrick Davey <patrick.davey@gmail.com>
2014-11-16 17:30:27 +1300

    Concatenator correctly concatenating all ruby files

--- spec/lib/scoreboard_rubywarrior/concatenator_spec.rb
+++ spec/lib/scoreboard_rubywarrior/concatenator_spec.rb
@@ -4,5 +4,10 @@ describe ScoreboardRubywarrior::Concatenator do
   let(:path) { File.expand_path('../../../fixtures/temp_ruby/', __FILE__) }
 
   it "should return a string representing all the files" do
+    content = ScoreboardRubywarrior::Concatenator.new(path).concatenate
+    expect(content).to match('def method_two')
+    expect(content).to match('class FileOne')
+    expect(content).to match('class FileTwo')
+    expect(content).to match('class FileThree')
   end
 end
