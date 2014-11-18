0667684225f7e8991de308ca9eef69fc60fbffef
Patrick Davey <patrick.davey@gmail.com>
2014-11-18 19:03:26 +1300

    Allowing a custom endpoint

--- lib/scoreboard_rubywarrior/reporter.rb
+++ lib/scoreboard_rubywarrior/reporter.rb
@@ -1,13 +1,28 @@
 module ScoreboardRubywarrior
   class Reporter
-    REPORTER_API_ENDPOINT_BASE_URI = 'http://chchruby-warrior.herokuapp.com/'
+    REPORTER_API_ENDPOINT_URI = 'http://chchruby-warrior.herokuapp.com/scores'
 
     include HTTParty
-    base_uri REPORTER_API_ENDPOINT_BASE_URI
+    base_uri REPORTER_API_ENDPOINT_URI
 
     def self.send_level_update(report)
-      post('/scores', body: report.to_json,
+      post(endpoint, body: report.to_json,
                 headers: { 'Content-Type' => 'application/json' })
+    end
+
+    def self.endpoint
+      cusom_endpoint || REPORTER_API_ENDPOINT_URI
+    end
+
+
+    private
+
+    def self.cusom_endpoint
+      endpoint_file = Pathname.new(Dir.pwd).join('.scoreboard_endpoint')
+      if File.exist?(endpoint_file)
+        return File.read(endpoint_file).strip
+      end
+      nil
     end
   end
 end
--- spec/lib/scoreboard_rubywarrior/reporter_spec.rb
+++ spec/lib/scoreboard_rubywarrior/reporter_spec.rb
@@ -1,24 +1,37 @@
 require 'spec_helper'
-
-describe ScoreboardRubywarrior::Reporter do
-  let(:report_hash) { {
-                        warrior_name: 'Trogdor',
-                        level_score: 10,
-                        time_bonus: 20,
-                        clear_bonus: 30,
-                        total_score: 60
+module ScoreboardRubywarrior
+  describe Reporter do
+    let(:report_hash) { {
+                          warrior_name: 'Trogdor',
+                          level_score: 10,
+                          time_bonus: 20,
+                          clear_bonus: 30,
+                          total_score: 60
+                        }
                       }
-                    }
 
-  before do
-    stub_request(:post, ScoreboardRubywarrior::Reporter::REPORTER_API_ENDPOINT_BASE_URI).
-    with(:body => "{\"level_score\":0,\"time_bonus\":0,\"total_score\":\"0\",\"warrior_name\":null}",
-         :headers => {'Content-Type'=>'application/json'}).
-    to_return(:status => 200, :body => "", :headers => {})
+    before do
+      stub_request(:post, Reporter::REPORTER_API_ENDPOINT_URI).
+      with(:body => "{\"level_score\":0,\"time_bonus\":0,\"total_score\":\"0\",\"warrior_name\":null}",
+           :headers => {'Content-Type'=>'application/json'}).
+      to_return(:status => 200, :body => "", :headers => {})
+    end
+
+    it "should post the details of a level" do
+      Reporter.should_receive(:post)
+      Reporter.send_level_update(report_hash)
+    end
+
+    describe 'endpoints' do
+      it "should have a default endpoint" do
+         Reporter.endpoint.should eq(Reporter::REPORTER_API_ENDPOINT_URI)
+      end
+    end
+
+    it "should be possible to specify a different endpoint" do
+      allow(Dir).to receive(:pwd).and_return(File.expand_path('../../../fixtures/', __FILE__) )
+       Reporter.endpoint.should eq('http://mytest.example.com/endpoint')
+    end
   end
 
-  it "should post the details of a level" do
-    ScoreboardRubywarrior::Reporter.should_receive(:post)
-    ScoreboardRubywarrior::Reporter.send_level_update(report_hash)
-  end
 end
